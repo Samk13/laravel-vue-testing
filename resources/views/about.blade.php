a41139863c0a163aadaa8212116e58513e143f3f
sam_arbid <samarbid13@users.noreply.github.com>
2019-12-14 02:41:40 +0100

    add controllers for pages

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+
+@extends('layout')
+
+@section('content')
+            <div class="title m-b-md">
+                about us 
+            </div>
+@endsection
+
+@section('text')
+            <p >
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nesciunt consequuntur veritatis, quis, assumenda est ipsa tempora illum similique laborum fugit explicabo ea incidunt dolorum doloribus ab saepe perferendis ratione.
+            </p>
+
+@endsection
+
+
