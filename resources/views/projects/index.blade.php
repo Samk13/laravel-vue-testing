3f5a0c74d08b7dba2f740f5cbd72625984f178dc
sam_arbid <samarbid13@users.noreply.github.com>
2020-01-03 01:47:58 +0100

    1-3-2020

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -16,30 +16,27 @@
   </div>
 </section>
 <div class="container is-fluid">
-    <div class="media">
-        <div class="level">
-
-        
-            
-             <div class="media-content">
-                @foreach ($projects as $project)
-            
-                <ul>
-                    <a  class="title is-1" href="/projects/{{ $project->id}}"
-                        <li>{{ $project->title }}</li>
-                    </a>
-                </ul>
-                @endforeach
-            <div class="field">
-      <p class="control">
-          
-                
-               <a class="button" href="/projects/{{ $project->id }}/edit">Edit</a>
-               <a class="button" href="/projects/create">add</a>
-                    </div>
-  </div>
+  <div class="media">
+    <div class="level">
+      <div class="media-content">
+        @foreach ($projects as $project)
+        <div class="is-ancestor">
+          <div class="tile is-parent">
+            <div class="tile is-child notification is-info">
+              <a class="tile" href="/projects/{{ $project->id}}" <li>{{ $project->title }}</li>
+              </a>
             </div>
+          </div>
         </div>
+        @endforeach
+        <div class="field">
+          <p class="control">
+            <a class="button" href="/projects/{{ $project->id }}/edit">Edit</a>
+            <a class="button" href="/projects/create">add</a>
+        </div>
+      </div>
     </div>
+  </div>
+</div>
 </div>
 @endsection
